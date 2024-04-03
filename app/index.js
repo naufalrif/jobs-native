42339dba56a6f40fa4d339cf3031a37bc66246a5
Naufal <nhabibie25@gmail.com>
2024-04-03 15:55:20 +0700

    Welcome, ScreenheaderBtn UI done

--- app/index.js
+++ app/index.js
@@ -13,8 +13,33 @@ const Home = () => {
             <Stack.Screen 
                 options={{
                     headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: " "
                 }}
             />
+
+            <ScrollView showsVerticalScrollIndicator={true}>
+                <View style={{
+                    flex: 1,
+                    padding: SIZES.medium,
+                    }}
+                >
+                    <Welcome />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+                    
+                </View>
+
+
+            </ScrollView>
+
         </SafeAreaView>
     )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,71 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList 
+} from 'react-native'
+
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants';
+
+const jobTypes = ['Fulltime', 'Parttime', 'Contractor']
 
 const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Fulltime') // untuk set highlighted button, default di 'full time'
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View>
+        <Text style={styles.userName}>Welcome</Text>
+        <Text style={styles.welcomeMessage}>Hello</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=''
+            onChange={() => {}}
+            placeholder='What are you looking for?'
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity> 
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity 
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {                // onclick ke tiap jenis job, cycle through the states, ganti highlighted jobs
+                setActiveJobType(item);
+                router.push('/search/${item}') // navigasi
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
+      </View>
+
+      
     </View>
   )
 }
